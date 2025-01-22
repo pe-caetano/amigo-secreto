728366f2b04d00b515f9e8a8c66ef144788caca6
Pedro Henrique <phcae@hotmail.com>
2025-01-22 07:55:05 -0300

    Sistema de Amigo Secreto

---
+++ app.js
@@ -0,0 +1,57 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeAmigos = [];
+
+function adicionarAmigo() {
+
+    let nomeAmigo = document.querySelector('input').value;
+
+    if (!nomeAmigo.length) {
+        return alert('Ops! Parece que você não inseriu um nome válido.');
+    }
+
+    if (listaDeAmigos.includes(nomeAmigo)) {
+        limparCampo();
+        return alert(`O nome de ${nomeAmigo} já está incluído na lista.`);
+    }
+
+    listaDeAmigos.push(nomeAmigo);
+    listarNomeInserido(nomeAmigo);
+    limparCampo();
+
+}
+
+function sortearAmigo() {
+
+    if (listaDeAmigos.length < 2) return alert('Por favor, insira no mínimo 2 nomes para realizar o sorteio.');
+
+    let escolherItem = Math.floor(Math.random() * listaDeAmigos.length);
+    let amigoSorteado = listaDeAmigos[escolherItem];
+
+    let campoSorteado = document.getElementById('resultado')
+    campoSorteado.innerHTML = '🎉 ' + amigoSorteado;
+
+    document.getElementById('sortear').setAttribute('disabled', true);
+    document.getElementById('adicionar').setAttribute('disabled', true);
+    document.getElementById('reiniciar').hidden = false;
+
+}
+
+function listarNomeInserido(nome) {
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML += `<li>${nome}</li>`;
+}
+
+function limparCampo() {
+    let campo = document.querySelector('input');
+    campo.value = '';
+}
+
+function reiniciarSorteio() {
+    listaDeAmigos = [];
+    document.getElementById('listaAmigos').innerHTML = '';
+    document.getElementById('resultado').innerHTML = '';
+
+    document.getElementById('sortear').removeAttribute('disabled');
+    document.getElementById('adicionar').removeAttribute('disabled');
+    document.getElementById('reiniciar').hidden = true;
+}
